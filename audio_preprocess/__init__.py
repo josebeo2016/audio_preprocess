e7a8fb7663d1af673de1c12139dae548f7e8278f
Long Nguyen-Vu <nve164@gmail.com>
2023-06-20 14:00:14 +0900

    finish class Dereverb, pytest

--- audio_preprocess/__init__.py
+++ audio_preprocess/__init__.py
@@ -1,5 +1,6 @@
 from .speechseperation import SpeechSeperate
 from .denoise_audio import DeNoise
+from .dereverb import Dereverb
 from .nonspeechtrim import NonspeechTrim
 from .__version__ import (
     
---
+++ tests/dereverb_audio_test.py
@@ -0,0 +1,23 @@
+import pytest
+import numpy as np
+from audio_preprocess.dereverb import Dereverb
+
+@pytest.fixture
+def dereverb():
+    return Dereverb()
+
+def test_process(dereverb):
+    # Test input signal
+    inputSignal = np.random.randn(16000)
+
+    # Process the input signal
+    outputSignal = dereverb.process(inputSignal)
+
+    # Check if the output signal has the correct shape
+    assert outputSignal.shape == inputSignal.shape
+
+    # Check if the output signal is not all zeros
+    assert not np.allclose(outputSignal, 0)
+
+    # Check if the output signal is of the correct data type
+    assert outputSignal.dtype == np.float32
